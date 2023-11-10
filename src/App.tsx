799f115f0fca598c7f1ef3f34ad4e94d4dff8f8e
lapinRagnar <lapinragnar@gmail.com>
2023-11-10 09:04:33 +0100

    button

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import MuiButton from './components/MuiButton'
 
 function App() {
 
@@ -7,7 +8,7 @@ function App() {
   return (
     <div>
       
-      salut
+      <MuiButton />
  
     </div>
   )
---
+++ src/components/MuiButton.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@mui/material"
+const MuiButton = () => {
+  return (
+    <div>
+      <Button variant="text">Text</Button>
+      <Button variant="contained">contained</Button>
+      <Button variant="outlined">Outlined</Button>
+
+    </div>
+  )
+}
+
+export default MuiButton
