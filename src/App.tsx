f707acf6691d65d7257911067b5041f32c95dc52
lapinRagnar <lapinragnar@gmail.com>
2023-11-10 08:51:30 +0100

    typography

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 
 import './App.css'
+import MuiTypography from './components/MuiTypography'
 
 function App() {
 
@@ -7,7 +8,7 @@ function App() {
   return (
     <div>
       
-      salut
+      <MuiTypography />
  
     </div>
   )
---
+++ src/components/MuiTypography.tsx
@@ -0,0 +1,24 @@
+import { Typography } from "@mui/material"
+
+const MuiTypography = () => {
+  return (
+    <div>
+      <Typography variant="h1">h1 heading</Typography>
+      <Typography variant="h2">h2 heading</Typography>
+      <Typography variant="h3">h3 heading</Typography>
+      <Typography variant="h4">h4 heading</Typography>
+      <Typography variant="h5">h5 heading</Typography>
+      <Typography variant="h6">h6 heading</Typography>
+
+      <Typography variant="subtitle1">subtile 1</Typography>
+      <Typography variant="subtitle2">subtile 2</Typography>
+
+      <Typography variant="body1"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque, illo. Nihil, omnis doloribus corrupti repellendus aspernatur assumenda minus id eius soluta veniam blanditiis, praesentium maiores quaerat iure optio ut. Soluta.</Typography>
+      <Typography variant="body2"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque, illo. Nihil, omnis doloribus corrupti repellendus aspernatur assumenda minus id eius soluta veniam blanditiis, praesentium maiores quaerat iure optio ut. Soluta.</Typography>
+
+      
+    </div>
+  )
+}
+
+export default MuiTypography
